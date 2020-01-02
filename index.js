538f276eb913d2e6077b0ee4fb4b78337f01a2f5
Alberto Filomeno <afilomeno@hcpci.com>
2020-01-02 15:51:56 -0500

    work-in-progress

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ const updateDocumentEvents = (updateAll) || (updateCommand && argv._.includes('d
 const updateDocumentPackets = (updateAll) || (updateCommand && argv._.includes('documentpackets'));
 const updateNoteTypes = (updateAll) || (updateCommand && argv._.includes('noteTypes'));
 const updateRatingEngines = (updateAll) || (updateCommand && argv._.includes('engines'));
+const updateCostEngineEquations = (updateAll) || (updateCommand && argv._.includes('costengineequations'));
+const updateCostEngineSelect = (updateAll) || (updateCommand && argv._.includes('costengineselect'));
+const updateCostFactorAdjustments = (updateAll) || (updateCommand && argv._.includes('costfactoradjustments'));
+const updateCostFactorDeductibles = (updateAll) || (updateCommand && argv._.includes('costfactordeductibles'));
+const updateCostFactors = (updateAll) || (updateCommand && argv._.includes('costfactors'));
 
 
 service.init({ logger: require('./lib/logger'), timeout: 0 });
@@ -52,6 +57,11 @@ Promise.all([
   updateDocumentEvents && require('./lib/update-documentevents').updateEvents(context),
   updateDocumentPackets && require('./lib/update-documentpackets').updatePackets(context),
   updateNoteTypes && require('./lib/update-noteTypes').updateNoteTypes(context),
-  updateRatingEngines && require('./lib/update-engines').updateRatingEngines(context)
+  updateRatingEngines && require('./lib/update-engines').updateRatingEngines(context),
+  updateCostEngineEquations && require('./lib/update-cost-engine').updateCostEngineEquations(context),
+  updateCostEngineSelect && require('./lib/update-cost-engine').updateCostEngineSelections(context),
+  updateCostFactorAdjustments && require('./lib/update-cost-engine').updateCostFactorAdjustments(context),
+  updateCostFactorDeductibles && require('./lib/update-cost-engine').updateCostFactorDeductibles(context),
+  updateCostFactors && require('./lib/update-cost-engine').updateCostFactors(context)
 ])
   .then(() => service.gracefulShutdown());
